9ff674137db46fef7b7b1909168ca87701e1129c
Gabriel Horner <gabriel.horner@gmail.com>
2014-06-10 23:20:48 -0400

    setup for tests

--- project.clj
+++ project.clj
@@ -1,2 +1,21 @@
 (defproject kukui "0.1.0-pre"
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2227"]]
+  :profiles {:dev {:plugins [[lein-cljsbuild "1.0.3"]
+                             [com.cemerick/clojurescript.test "0.3.1"]]
+                   :hooks        [leiningen.cljsbuild]}}
+  :cljsbuild {:builds
+              {:whitespace {:source-paths ["test"]
+                            :compiler
+                            {:output-to "target/cljsbuild/whitespace/kukui.js"
+                             :optimizations :whitespace
+                             :pretty-print true}}
+               :advanced {:source-paths ["test"]
+                          :compiler
+                          {:output-to "target/cljsbuild/advanced/kukui.js"
+                           :pretty-print  false
+                           :optimizations :advanced}}}
+              :test-commands {"whitespace" ["phantomjs" :runner
+                                            "target/cljsbuild/whitespace/kukui.js"]
+                              "advanced" ["phantomjs" :runner
+                                          "target/cljsbuild/advanced/kukui.js"]}})
---
+++ test/lt/plugins/kukui-test.cljs
@@ -0,0 +1,6 @@
+(ns lt.plugins.kukui-test
+  (:require-macros [cemerick.cljs.test :refer [is deftest]])
+  (:require [cemerick.cljs.test :as t]))
+
+(deftest tests-work
+  (is (= 2 (inc 1))))
