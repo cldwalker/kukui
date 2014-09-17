edc3230151139c2eb20c6465e777c625bafe955e
Gabriel Horner <gabriel.horner@gmail.com>
2014-09-16 20:39:36 -0400

    Add rule name linting - fail fast when invalid

--- src/lt/plugins/kukui/datascript.cljs
+++ src/lt/plugins/kukui/datascript.cljs
@@ -1,6 +1,7 @@
 (ns lt.plugins.kukui.datascript
   "Datascript helper fns"
-  (:require [datascript :as d])
+  (:require [datascript :as d]
+            [clojure.set :as cset])
   (:refer-clojure :exclude [count]))
 
 (def conn "A datascript connection" nil)
@@ -18,7 +19,35 @@
 (defn transact! [records]
   (d/transact! conn records))
 
+;; thanks to datascript
+(defn parse-query [query]
+  (loop [parsed {} key nil qs query]
+    (if-let [q (first qs)]
+      (if (keyword? q)
+        (recur parsed q (next qs))
+        (recur (update-in parsed [key] (fnil conj []) q) key (next qs)))
+      parsed)))
+
+
+(defn lint-rules*
+  "Returns set of invalid rules for a given query and all its args (which include rules)"
+  [query args]
+  (let [query-map (d/parse-query query)]
+    (when (= '% (second (:in query-map)))
+      (let [user-rules (set (keep #(when (list? %) (first %)) (:where query-map)))
+            valid-rules (set (map ffirst (first args)))]
+        (cset/difference user-rules valid-rules)))))
+
+(defn lint-rules
+  "Throws an error if rule is invalid in :where clause. Any where clause that is list
+  qualifies as a rule. Consider moving into dlint."
+  [query args]
+  (when-let [invalid (seq (lint-rules* query args))]
+    (throw (ex-info (str "Following rules are invalid: " invalid) {}))))
+
 (defn q [query & args]
+  (assert (or (map? query) (vector? query)))
+  (lint-rules query args)
   (apply d/q query @conn args))
 
 (defn qf [query & args]
--- test/lt/plugins/kukui/db_test.cljs
+++ test/lt/plugins/kukui/db_test.cljs
@@ -2,6 +2,7 @@
   (:require-macros [cemerick.cljs.test :refer [is deftest testing]])
   (:require [cemerick.cljs.test :as t]
             [dlint.core :refer [lint]]
+            [lt.plugins.kukui.datascript :as d]
             [lt.plugins.kukui.db :as db]))
 
 (deftest rules-lint-valid
@@ -11,3 +12,11 @@
   (doseq [[query-name query] db/named-queries]
     (is (= {} (lint query))
         (str "Query " query-name " should be valid"))))
+
+(deftest lint-rules-finds-invalid
+  (is
+   (let [query '[:find ?e
+                 :in $ % ?input-tag ?ent-type
+                 :where (taged-with ?e ?input-tag) [?e :type ?ent-type]]
+         args [db/rules :arg1 :arg2]]
+     (= #{'taged-with} (d/lint-rules* query args)))))
